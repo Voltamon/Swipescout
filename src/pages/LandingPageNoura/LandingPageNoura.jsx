691c7df99f06416593230163d511d2c759ade7b8
obaidofj <obaidofj@gmail.com>
2025-04-12 08:42:00 +0200

    edit ring icon and space

--- src/pages/LandingPageNoura/LandingPageNoura.jsx
+++ src/pages/LandingPageNoura/LandingPageNoura.jsx
@@ -44,11 +44,11 @@ const LandingPageNoura = () => {
             <img src={phoneImage} alt="App Preview" />
           </div>
           <div className="features-text">
-            <Feature  title="Video Resumes" description="
+            <Feature title="Video Resumes" description="
 🔔 Showcase Personality in Seconds
 SwipeScout lets job seekers express themselves with short 15–45 second video pitches—making it easier for companies to connect with confident, creative talent." />
 
-            <Feature style={{ marginLeft: width >= 1025 ? 60 : -8 }} className="feature-container" title="Swipe Feature" description="🔔 Swipe Feature
+            <Feature style={{ marginLeft: width >= 1025 ? 60 : -8 }} className="feature-container" title="Swipe Feature" description="🔔 Swipe Featuregit
 Fast, Fun, and Intentional Matching
 Swipe through jobs or candidates just like you would on social apps. Tap in for full profiles, then connect if there’s mutual interest—no ghosting, no clutter." />
           </div>
@@ -56,11 +56,11 @@ Swipe through jobs or candidates just like you would on social apps. Tap in for 
       </div>
       <div style={{ background: "#ffffff" }}>
         <div className="feature-grid" style={{ margin: "50px" }}>
-          <FeatureCard icon={<LuArrowLeftRight />} title={"Video Resumes"} subtitle={"Unlock Real Talent"}  description={" SwipeScout lets job seekers upload 15–45 second video resumes, giving employers a real glimpse of their personality, communication, and creativity—far beyond what paper resumes can offer."} />
+          <FeatureCard icon={<LuArrowLeftRight />} title={"Video Resumes"} subtitle={"🔔 Unlock Real Talent"} description={" SwipeScout lets job seekers upload 15–45 second video resumes, giving employers a real glimpse of their personality, communication, and creativity—far beyond what paper resumes can offer."} />
 
-          <FeatureCard icon={<LuArrowLeftRight />} title={"Swipe to Discover"} subtitle={"Smarter Matching"}  description={" Our intuitive swipe interface allows job seekers and employers to discover each other quickly, creating matches based on real impressions, not just text."} />
+          <FeatureCard icon={<LuArrowLeftRight />} title={"Swipe to Discover"} subtitle={"🔔 Smarter Matching"} description={" Our intuitive swipe interface allows job seekers and employers to discover each other quickly, creating matches based on real impressions, not just text."} />
 
-          <FeatureCard icon={<LuArrowLeftRight />} title={"Real-Time Connections"} subtitle={"Chat and Connect"}  description={" Once there’s a match, messaging opens up—making it easy to schedule interviews, ask questions, and take the next step, all in-app."} />
+          <FeatureCard icon={<LuArrowLeftRight />} title={"Real-Time Connections"} subtitle={"🔔 Chat and Connect"} description={" Once there’s a match, messaging opens up—making it easy to schedule interviews, ask questions, and take the next step, all in-app."} />
         </div>
         {/* <h2 style={{ color: "black" }}>SwipeScout News </h2>
         <p style={{ textAlign: "center", color: "black" }}>
@@ -71,8 +71,7 @@ Swipe through jobs or candidates just like you would on social apps. Tap in for 
           <TestimonialCard name={"The Snap Pixel: How It Works and How to Install "} role={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra nunc ante velit vitae. Est tellus vitae, nullam lobortis enim."} testimonial={""} image={news2} />
           <TestimonialCard name={"The Snap Pixel: How It Works and How to Install "} role={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra nunc ante velit vitae. Est tellus vitae, nullam lobortis enim."} testimonial={""} image={news3} />
         </div> */}
-        <br>
-        </br>
+        <br />
         <br />{" "}
       </div>
 
