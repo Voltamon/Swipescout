cdfe6c45e7d126dde9da8b19d989e2537a74daf2
obaid <obaidofj@gmail.com>
2025-04-17 17:27:01 +0300

    fix for build5

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,7 +1,7 @@
 
 
 import React from "react";
-import Navbar from "../../components/Navbar";
+import Navbar from "../../components/Navigationbar";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import FeatureCard from "../../components/FeatureCard";
