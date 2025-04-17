b101c342c711ea02cf9f33f1f7eeb1a24b15af69
obaid <obaidofj@gmail.com>
2025-04-17 17:24:24 +0300

    fix for build4

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,17 +1,11 @@
 import { Link } from 'react-router-dom';
-import './Navbar.css'; // optional for styling
+import './Navbar.css'; 
 
 
 const Navbar = () => {
   return <div>
       <nav className="navbar">
-        {/* <Link to="/">Landing</Link>
-        {/* <Link to="/login">Login</Link>
-      <Link to="/signup">Signup</Link>
-      <Link to="/Home2">Home2</Link>
-      <Link to="/explore-jobs">Explore Jobs</Link>
-      <Link to="/my-applications">My Applications</Link>
-      <Link to="/profile">Profile</Link> */}
+ 
         <Link to="/auth-page">Login Page</Link>
         <Link to="/video-feed">Video Feed</Link>
         <Link to="/Employer-explore-sidebar">Job Seekers Videos Style</Link>
@@ -21,7 +15,7 @@ const Navbar = () => {
       </nav>
       <hr />
       <nav className="navbar">
-        {/* <Link to="/PagesNavigation">App Page</Link> */}//
+      
         <Link to="/register-form">Register Form</Link>
         <Link to="/login-form">Login Form</Link>
         <Link to="/dashboard-jobseeker">Dashboard Jobseeker1</Link>
@@ -39,9 +33,9 @@ const Navbar = () => {
         <Link to="/candidate-search">Search for a Job Seeker</Link>
         <Link to="/post-job">Post Job2</Link>
         <Link to="/job/1">Job Details</Link> {/* example ID */}
-        {/*<Link to="/search">Search for Jobs</Link>*/}
+     
         <Link to="/admin-dashboard">Admin Dashboard</Link>
-        {/* <Link to="/auth-page2">Auth Page 2</Link> */}
+
       </nav>
       <hr />
     </div>;
