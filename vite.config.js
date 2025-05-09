a4155e75f3d6c8cf1ed6f60fab58436da3b9d648
obaid <obaidofj@gmail.com>
2025-05-10 00:21:20 +0300

    checking erro d

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     include: ['react-linkedin-login-oauth2']
+//   },
+// })
+
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    include: ['react-linkedin-login-oauth2']
+  build: {
+    sourcemap: true, // or 'hidden' if you don't need sourcemaps
   },
+  plugins: [react()],
 })
